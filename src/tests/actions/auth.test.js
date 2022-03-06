c466c784bac234fdb499154b72d7acdde0aa1f25
Carlos Pereira <carloscoto0103@gmail.com>
2022-03-06 16:32:38 -0600

    auth tests added firebase

--- src/tests/actions/auth.test.js
+++ src/tests/actions/auth.test.js
@@ -1,8 +1,21 @@
-import { login, logout } from "../../actions/auth";
-import { types } from "../../types/types";
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 
+import { login, logout, startLoginEmailPassword, startLogout } from '../../actions/auth';
+import { types } from '../../types/types';
+
+const middlewares = [ thunk ];
+const mockStore = configureStore(middlewares);
+
+const initialState = {};
+
+let store = mockStore( initialState );
 
 describe('Auth tests', () => {
+
+    beforeEach(() => {
+        store = mockStore( initialState );
+    });
 
     test('login and logout should create a action respectively', () => {
 
@@ -26,10 +39,37 @@ describe('Auth tests', () => {
 
     });
 
-    test('should logout', () => {
+    test('should logout startLogout', async () => {
 
-        
+        await store.dispatch( startLogout() );
+
+        const actions = store.getActions();
+
+        expect( actions[0] ).toEqual({
+            type: types.logout
+        });
+
+        expect( actions[1] ).toEqual({
+            type: types.notesLogoutCleaning
+        });
 
     });
+
+    test('should start the Login with email and password', async () => {
+
+        await store.dispatch( startLoginEmailPassword('test@testing.com', '123456' ) );
+
+        const actions = store.getActions();
+        
+        expect( actions[1] ).toEqual({
+            type: types.login,
+            payload: {
+                uid: 'Y2KED95lUWW0ZaGAIJK8Aq3grpg1',
+                displayName: null
+            }
+        });
+        
+
+    })
 
 });
